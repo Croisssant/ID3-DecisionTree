eccc8fbb981ca94626da66ebd7fd8212a13dcbfa
Kai Wey <limkaiwey@yahoo.com>
2020-11-21 23:47:38 +0000

    Added data and data loader

--- Classification.m
+++ Classification.m
@@ -1,1 +1,13 @@
-aa
+T = readtable('.\Data\Wine\refined_data.csv', 'PreserveVariableNames', true)
+
+data = T(randperm(size(T,1)), :)
+
+training_x = data(1:120, 1:13)
+training_y = data(1:120, 14)
+
+testing_x = data(120:end, 1:13)
+testing_y = data(120:end, 14)
+
+function decision_tree = DECISION-TREE-LEARNING(features, labels)
+    entropy = -p/(p+n) * log2(p/(p+n)) - n/(p+n) * log2(n/(p+n))
+    
--- Regression.m
+++ Regression.m
@@ -1,1 +1,8 @@
+data = readtable('.\Data\Airfoil_self_noise\airfoil_self_noise.dat', 'PreserveVariableNames', true)
 
+
+% training_x = data(1:120, 1:13)
+% training_y = data(1:120, 14)
+% 
+% testing_x = data(120:end, 1:13)
+% testing_y = data(120:end, 14)
